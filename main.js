765cdd955d584211ee7d88f11f987f279bb7a63b
IlyaMinchenko <sir.over@yandex.ru>
2021-06-06 00:30:53 +0500

    Homework3

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const scorpion = {
+const $arena = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+
+const player1 = {
+    player : 1,
     name : 'SCORPION',
-    hp : 10,
+    hp : 100,
     img : 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon : ['Kunai'],
     attack : function attack() {
@@ -8,9 +12,10 @@ const scorpion = {
     },
 };
 
-const subzero = {
+const player2 = {
+    player: 2,
     name : 'SUB-ZERO',
-    hp : 70,
+    hp : 100,
     img : 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon : ['IceBall'],
     attack : function attack() {
@@ -18,30 +23,59 @@ const subzero = {
     },
 };
 
-function createPlayer(name, player) {
-    const $arena = document.querySelector('.arenas');
-    const $player = document.createElement('div');
-    const $progressbar = document.createElement('div');
-    const $life = document.createElement('div');
-    const $name = document.createElement('div');
-    const $character = document.createElement('div');
-    const $img = document.createElement('img');
-
-$arena.appendChild($player);
-    $player.classList.add(name); //<'name'>...</'name'>
-    $player.appendChild($progressbar);
-        $progressbar.classList.add('progressbar') //<progressbar>...</progressbar>
-        $progressbar.appendChild($life);
-            $life.classList.add('life'); //<life>...</life>
-            $life.style.width = player.hp + '%'; //Значение здоровья у персонажей
-        $progressbar.appendChild($name);
-            $name.classList.add('name'); //<name>...</name>
-            $name.innerText = (player.name); //Имена персонажей
-    $player.appendChild($character);
-        $character.classList.add('character'); // <character>...</character>
-        $character.appendChild($img);
-            $img.src = player.img; //Изображение персонажа
+function createElement(tag, className) {
+    const $tag = document.createElement(tag);
+    if (className) {
+        $tag.classList.add(className);
+    }
+    return $tag;
 }
 
-createPlayer('player1', scorpion);
-createPlayer('player2', subzero);
+function createPlayer(playerObj) {
+    const $player = createElement('div', 'player' + playerObj.player);
+    const $progressbar = createElement('div', 'progressbar');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $character = createElement('div', 'character');
+    const $img = createElement('img');
+
+    $player.appendChild($progressbar);
+    $progressbar.appendChild($life);
+    $life.style.width = playerObj.hp + '%'; //Значение здоровья у персонажей
+    $progressbar.appendChild($name);
+    $name.innerText = (playerObj.name); //Имена персонажей
+    $player.appendChild($character);
+    $character.appendChild($img);
+    $img.src = playerObj.img;
+    return $player
+ }
+
+function changeHP(player){
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+    player.hp -= Math.random() * 20;
+    console.log(player.hp)
+    $playerLife.style.width = player.hp + '%';
+    if (player.hp < 0) {
+        if (player.name == player1.name) {
+            $arena.appendChild(playerWin(player2.name));
+        } else{
+        $arena.appendChild(playerWin(player1.name));
+        }
+        player.hp = 0;
+        $randomButton.disabled = true;
+    }
+}
+
+function playerWin(name) {
+    const $WinTitle = createElement('dib', 'WinTitle');
+    $WinTitle.innerText = name + ' WIN';
+    return $WinTitle;
+}
+
+ $randomButton.addEventListener('click', function () {
+     changeHP(player1);
+     changeHP(player2);
+ })
+
+ $arena.appendChild(createPlayer(player1));
+ $arena.appendChild(createPlayer(player2));
