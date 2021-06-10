31e492d03149aa9f1e75a930b4d80be85b6671f4
IlyaMinchenko <sir.over@yandex.ru>
2021-06-10 13:30:48 +0500

    HomeWork5

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const $arena = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
-
+const $formFight = document.querySelector('.control')
 const player1 = {
     player : 1,
     name : 'SCORPION',
@@ -29,6 +28,16 @@ const player2 = {
     renderHP: renderHP,
 };
 
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
+
+function removeArena(tag){
+    $arena.removeChild(document.querySelector(tag));
+}
 function createElement(tag, className) {
     const $tag = document.createElement(tag);
     if (className) {
@@ -62,7 +71,9 @@ function createPlayer(playerObj) {
 
 function changeHP(num){
     this.hp -= num;
-    this.renderHP();
+    if (this.hp <= 0) {
+        this.hp = 0;
+    }
 }
 
 function elHP() {
@@ -70,11 +81,7 @@ function elHP() {
 }
 
 function renderHP() {
-    if (this.hp <= 0) {
-        this.hp = 0;
-    }
     this.elHP().style.width = this.hp + '%';
-
 }
 
 function playerWin(name) {
@@ -87,24 +94,9 @@ function playerWin(name) {
     return $WinTitle;
 }
 
- $randomButton.addEventListener('click', function () {
-     player1.changeHP(getRandom(20));
-     player2.changeHP(getRandom(20));
-     if (player1.hp === 0 || player2.hp === 0){
-        $randomButton.disabled = true;
-        createReloadButton();
-     }
-
-     if (player1.hp === 0  && player1.hp < player2.hp){
-         $arena.appendChild(playerWin(player2.name));
-     } else if (player2.hp === 0  && player2.hp < player1.hp){
-        $arena.appendChild(playerWin(player1.name));
-    } else if (player1.hp === 0  && player1.hp === 0){
-        $arena.appendChild(playerWin());
-    }
- })
 
  function createReloadButton() {
+     removeArena('.control');
      const $reloadWrap = createElement('div', 'reloadWrap');
      const $buttonReload = createElement('button', 'button')
      $buttonReload.innerText = 'Restart';
@@ -117,5 +109,50 @@ function playerWin(name) {
 
  $arena.appendChild(createPlayer(player1));
  $arena.appendChild(createPlayer(player2));
+
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence,
+    }
+}
+
+ $formFight.addEventListener('submit', function(e) {
+     e.preventDefault();
+     const enemy = enemyAttack();
+     const attack = {};
+    
+     for (let item of $formFight) {
+         if (item.checked && item.name === 'hit') {
+             attack.value = getRandom(HIT[item.value]);
+             attack.hit = item.value;
+         }
+
+         if (item.checked && item.name === 'defence') {
+             attack.defence = item.value;
+         }
+         item.checked = false;
+     }
+
+     player1.changeHP(getRandom(20));
+     player1.renderHP();
+     player2.changeHP(getRandom(20));
+     player2.renderHP();
+     if (player1.hp === 0 || player2.hp === 0){
+        createReloadButton();
+
+     }
+
+     if (player1.hp === 0  && player1.hp < player2.hp){
+         $arena.appendChild(playerWin(player2.name));
+     } else if (player2.hp === 0  && player2.hp < player1.hp){
+        $arena.appendChild(playerWin(player1.name));
+    } else if (player1.hp === 0  && player1.hp === 0){
+        $arena.appendChild(playerWin());
+    }
+ })
 
 
